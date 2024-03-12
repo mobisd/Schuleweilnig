92e2522a223042b4dac956e9e3a52bf9a5d3215e
Spolyc <darijo080306@gmail.com>
2024-03-12 14:12:07 +0100

    list js blablabla
    hab angefangen js für die Klassenliste zu machen

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Document</title>
+    <link rel="stylesheet" href="css/index-style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+<body>
+<!-- navbar -->
+<div class="nav">
+    <!-- Left Navbar Part -->
+    <div class="nav-user">
+        <div class="dropdown">
+         <button class="dropbtn user">Darijo Popov</button>
+            <div class="dropdown-content">
+                <div class="space"></div>
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+            </div>
+        </div>
+    </div>
+    <!-- Right Navbar Part -->
+    <div class="nav-name">
+        
+    </div>
+</div>
+    
+
+
+<div class="welcome">
+
+</div>
+
+<div class="main">
+    <div class="left-nav">
+        <div class="left-nav-block">
+            <div class="drop-cont layer-1">
+                <p>AIF</p>
+                <div class="drop-cont layer-1-content layer-2">
+                    <p>2AAIF</p>
+                    <div class="drop-cont layer-2-content layer-3">
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="drop-cont layer-1">
+                <div class="drop-cont layer-2">
+                    <div class="drop-cont layer-3">
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="drop-cont layer-1">
+                <div class="drop-cont layer-1-content layer-2">
+                    <div class="drop-cont layer-2-content layer-3">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="courses">
+        <div class="courses-block">
+a
+        </div>
+    </div> 
+</div>
+
+
+
+<div class="calender">
+
+</div>
+
+<div class="corner-nav">
+
+</div>
+
+</body>
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="js/index-scripts.js" ></script>
+</html>
